198d6af1a717549ccb0adf56f2edde89b055bdf5
Corinna28 <lunacleju@gmail.com>
2023-07-18 15:44:08 +0200

    QCM

---
+++ rgpdqcm.php
@@ -0,0 +1,69 @@
+
+
+Qu’est-ce qu’une donnée à caractère personnel ? <A></A>
+
+    a. Une information relative à une personne physique ou morale
+    b. Une information relative à une personne physique uniquement
+    c. Une information relative à une personne morale uniquement
+
+Quels sont les huit règles d’or RGPD que tout organisme se doit de respecter ? A / B / D / E/ G / H / J / K
+
+    a. La licéité des traitements
+    b. La finalité des traitements
+    c. La confidentialité des données de la personne morale
+    d. La minimisation des données
+    e. La protection particulière des données sensibles
+    f. La qualité des processus
+    g. La durée de conservation limitée des données
+    h. Les mesures de sécurité des données
+    i. La multiplicité des bases de données
+    j. La transparence
+    k. Le droit des personnes
+    l. La vente des bases de données
+
+Savez-vous comment le consentement d’un utilisateur de recevoir de la publicité par email doit être obtenu ? C
+
+    a. S’il est évoqué dans les conditions générales du site
+    b. S’il est précisé dans les conditions d’acceptation du service ou contrat
+    c. S’il est collecté de façon spécifique par le biais d’une case à cocher
+
+Est-il possible d’utiliser les données pour autre chose que leur finalité initiale sans en informer les personnes ? B
+
+    a. Oui
+    b. Non
+
+Est-il possible de collecter le numéro de sécurité sociale de n’importe qui ? <B></B>
+
+    a. Oui
+    b. Non
+
+Dans les catégories de données suivantes, laquelle ne fait pas partie de la définition des données sensibles du RGPD mais doit quand même être encadrée comme une catégorie sensible ? A
+
+    a. Les données de santé
+    b. Les données d’infraction et de condamnation pénale
+    c. Les données sur la prétendue origine ethnique ou raciale
+    d. Les données sur l’opinion religieuse ou philosophique
+
+Est-il obligatoire de mettre en place des durées de conservation pour tous les traitements de données ? A
+
+    a. Oui
+    b. Non
+
+Nommez les 3 principes de la sécurité des données
+
+    a. Confidentialité
+    b. Gratuité
+    c. Intégrité
+    d. Disponibilité
+    e. Honnêteté
+
+Est-ce qu’il est obligatoire d’informer les personnes de ce que vous faites de leur données avant de les collecter ? A
+
+    a. Oui
+    b. Non
+
+De combien de temps disposez-vous pour répondre à une demande d’exercice de droits d’un utilisateur/clients/usagers sur ses données personnelles ? C
+
+    a. 24h
+    b. Une semaine
+    c. Un mois
